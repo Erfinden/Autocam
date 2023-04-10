0a33cac4bed6677553fcbdcb35be2783bace7456
Das-Kampfnudelholz <102672314+Das-Kampfnudelholz@users.noreply.github.com>
2023-04-10 13:49:12 +0200

    Update autocam.py

--- autocam.py
+++ autocam.py
@@ -2,8 +2,21 @@ import cv2
 import time
 import shutil
 import os
-import ansyncio
+import asyncio
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+#### Colors for console Output
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+##########
 
 #### Setting up the Camara Resolution and Connection
 cap = cv2.VideoCapture(0)
@@ -11,9 +24,25 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 ###########
 
+#### Create Config file if it dosn't exsist 
+if not 'Config.json' in os.listdir():
+    open('Config.json', 'w').write('{"usb:"True","save_direct":"/home/pi/Desktop/images/","file_prefix":"image_"}')
+    print(bcolors.WARNING + "Warning: Config has been created. Restart The bot after filling info" + bcolors.ENDC)
+############
+
+#### Read in Config File
+ConfigFile = json.loads(open("Config.json", "r").read())
+############
+
+if (ConfigFile['usb'] == "True"):
+    USBSave = True
+
 #### Setting the Variables for the Filename and the Folder
-directory = "/home/pi/Desktop/images/"
-file_prefix = "image_"
+if (USBSave == True):
+    usb_path = "/media/pi/"
+directory = ConfigFile['save_direct']
+file_prefix = ConfigFile['file_prefix']
+running = True
 ############
 
 ##### Check if the Folder already exsists, and if not create it
@@ -22,7 +51,6 @@ if not os.path.exists(directory):
 #############
 
 #### Settig up USB as a storage solution
-usb_path = "/media/pi/"
 usb_folder = next((folder for folder in os.listdir(usb_path) if os.path.isdir(os.path.join(usb_path, folder))), None)
 if usb_folder:
     usb_directory = os.path.join(usb_path, usb_folder, "images")
@@ -33,7 +61,7 @@ else:
 ########
 
 #### Making the Main Loop for the Capturing
-while True:
+while (running == True):
     try:
         # Get The current Timestamp 
         current_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
@@ -50,12 +78,14 @@ while True:
             usb_file_name = os.path.join(usb_directory, file_prefix + current_time + ".jpg")
             shutil.copy(file_name, usb_file_name)
 
-        # Let the code sleep for 1800 Secconds (30 Minutes)
+        # Let the code sleep for 1800 Secconds (30Minutes)
+        print(bcolors.OKGREEN + "Image Captured at {current_time} " + bcolors.ENDC)
         time.sleep(1800)
 
     except cv2.error:
-        print("Error: Failed to capture frame from camera.")
+        print(bcolors.FAIL + "Error: Failed to capture frame from camera." + bcolors.ENDC)
         
     except PermissionError:
-        print("Error: Permission denied. USB drive not found.")
+        print(bcolors.FAIL + "Error: Permission denied. USB drive not found." + bcolors.ENDC)
         pass
+
