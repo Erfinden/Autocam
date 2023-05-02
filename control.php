05b3aab50200b407e24107b53a513bf988d69662
nanocraftmr <128908840+nanocraftmr@users.noreply.github.com>
2023-05-03 01:31:55 +0200

    better control.php

--- control.php
+++ control.php
@@ -2,7 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $action = $_POST['action'];
   if ($action == 'start') {
-    $output = shell_exec('sudo /usr/bin/python /var/www/html/autocam/autocam.py');
+    $output = shell_exec('sudo /usr/bin/python3 /var/www/html/autocam/autocam.py > /dev/null 2>&1 &');
     echo "Started process $output";
   } elseif ($action == 'stop') {
     $output = shell_exec('sudo pkill -f autocam.py');
