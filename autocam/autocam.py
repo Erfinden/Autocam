b4fd9112120f3703f90fa363261fdb12e05fe68a
Neptun <128908840+nanocraftmr@users.noreply.github.com>
2023-05-14 22:21:46 +0200

    upload to server for further use

--- autocam/autocam.py
+++ autocam/autocam.py
@@ -2,19 +2,21 @@ import subprocess
 import time
 import os
 import json
+import requests
 
+# Load the configuration file
 with open('/var/www/html/config.json', 'r') as f:
     config = json.load(f)
 
-# set up the file name and directory for the images
+# Set up the file name and directory for the images
 directory = config['img_dir']
 file_prefix = "image_"
 
-# check if the images directory exists, and create it if it doesn't
+# Check if the images directory exists, and create it if it doesn't
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-# set up the USB drive backup
+# Set up the USB drive backup
 if config.get('usb_backup', False):
     usb_path = config['usb_path']
     usb_folder = next((folder for folder in os.listdir(usb_path) if os.path.isdir(os.path.join(usb_path, folder))), None)
@@ -29,19 +31,29 @@ else:
 
 while True:
     try:
-        # get the current time
+        # Get the current time
         current_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
 
-        # capture an image using raspistill command
+        # Capture an image using raspistill command
         file_name = os.path.join(directory, file_prefix + current_time + ".jpg")
         subprocess.call(["raspistill", "-n", "-ex", "night", "-o", file_name])
 
-        # backup the file to USB if it is connected
+        # Backup the file to USB if it is connected
         if usb_directory:
             usb_file_name = os.path.join(usb_directory, file_prefix + current_time + ".jpg")
             shutil.copy(file_name, usb_file_name)
 
-        # wait for 30 minutes before taking the next picture
+        # Upload the last captured image to the server
+        files = {'image': open(file_name, 'rb')}
+        response = requests.post(config['server_url'], files=files)
+
+        # Check the response status
+        if response.status_code == 200:
+            print('Image uploaded successfully!')
+        else:
+            print('Failed to upload image: %s' % response.text)
+
+        # Wait for the specified time before taking the next picture
         time.sleep(int(config['sleep']))
 
     except OSError:
