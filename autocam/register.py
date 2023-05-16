771aa73ef52c7e7e88475a5d3cc6802b26249cb0
Neptun <128908840+nanocraftmr@users.noreply.github.com>
2023-05-17 01:59:56 +0200

    bug fixes

--- autocam/register.py
+++ autocam/register.py
@@ -1,19 +1,19 @@
 import requests
 import json
 
-def update_config(username, password):
+def update_config(username, password, server_url):
     config_file = '/var/www/html/config.json'
     with open(config_file, 'r') as f:
         config = json.load(f)
-    
+
     config['username'] = username
     config['password'] = password
 
     with open(config_file, 'w') as f:
         json.dump(config, f, indent=4)
 
-def register_user(username, password):
-    url = 'http://192.168.188.193:5000/register'
+def register_user(username, password, server_url):
+    url = server_url + '/register'
     data = {'username': username, 'password': password}
     response = requests.post(url, data=data)
     return response.text
@@ -22,6 +22,12 @@ def register_user(username, password):
 username = input("Username: ")
 password = input("Password: ")
 
-update_config(username, password)
-response = register_user(username, password)
+# Retrieve the server_url from your config file
+config_file = '/var/www/html/config.json'
+with open(config_file, 'r') as f:
+    config = json.load(f)
+    server_url = config['server_url']
+
+update_config(username, password, server_url)
+response = register_user(username, password, server_url)
 print(response)  # Print the response from the server
