1b6d0c51ed174cf820a1ff79d0871c5297836a4e
Neptun <128908840+nanocraftmr@users.noreply.github.com>
2023-05-18 23:53:10 +0200

    fixed

--- autocam/register.py
+++ autocam/register.py
@@ -1,23 +1,35 @@
 import requests
 import json
 
-def remove_user(config, username, password):
-    url = config['server_url'] + '/remove'
+def update_config(username, password, server_url):
+    config_file = '/var/www/html/config.json'
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+
+    config['username'] = username
+    config['password'] = password
+
+    with open(config_file, 'w') as f:
+        json.dump(config, f, indent=4)
+
+def register_user(config, username, password):
+    server_url = config['server_url']
+    url = server_url + '/register'
     data = {'username': username, 'password': password}
     response = requests.post(url, data=data)
     return response.text
 
-# Load the configuration file
-with open('/var/www/html/config.json', 'r') as f:
+# Example usage
+config_file = '/var/www/html/config.json'
+with open(config_file, 'r') as f:
     config = json.load(f)
 
-# Example usage
 username = input("Username: ")
 password = input("Password: ")
-confirmation = input("Are you sure you want to delete your account? (y/n): ")
 
-if confirmation.lower() == 'y':
-    response = remove_user(config, username, password)
-    print(response)
+response = register_user(config, username, password)
+if response == 'User registered successfully.':
+    update_config(username, password, config['server_url'])
+    print('User registered successfully! Config file updated.')
 else:
-    print("Account deletion canceled.")
+    print('User registration failed:', response)
