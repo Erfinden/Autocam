73096a2219cdda881e6750b53bd6332843cf2cf9
Neptun <128908840+nanocraftmr@users.noreply.github.com>
2023-06-21 00:10:06 +0200

    nice new design + config file now editable

--- index.php
+++ index.php
@@ -1,95 +1,298 @@
-import subprocess
-import time
-import os
-import json
-import requests
-import shutil
-import sched
-from requests.exceptions import Timeout, ConnectionError, InvalidSchema
-
-def capture_and_upload_image(config, scheduler):
-    directory = config['img_dir']
-    file_prefix = "image_"
-
-    if config.get('save_to_drive', True):
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-    if config.get('usb_backup', False):
-        usb_path = config['usb_path']
-        usb_folder = next((folder for folder in os.listdir(usb_path) if os.path.isdir(os.path.join(usb_path, folder))), None)
-        if usb_folder:
-            usb_directory = os.path.join(usb_path, usb_folder, "images")
-            if not os.path.exists(usb_directory):
-                os.makedirs(usb_directory)
-        else:
-            usb_directory = None
-    else:
-        usb_directory = None
-
-    try:
-        current_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.gmtime())
-
-        file_name = os.path.join(directory, file_prefix + current_time + ".jpg")
-        video_device = config.get("video_device", "video0")  # Default to "video0" if not specified
-        video_device_path = "/dev/" + video_device
-
-        # Attempt to capture the image using fswebcam command
-        try:
-            subprocess.call(["fswebcam", "--no-banner", "-d", video_device_path, "-S", "2", file_name])
-        except OSError:
-            pass  # Ignore the "Failed to capture image using fswebcam command" error
-
-        if usb_directory:
-            usb_file_name = os.path.join(usb_directory, file_prefix + current_time + ".jpg")
-            shutil.copy(file_name, usb_file_name)
-
-        # Upload the image
-        files = {'image': open(file_name, 'rb')}
-        data = {
-            'key': config['key']
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Autocam Webserver</title>
+    <script>
+        setInterval(function(){
+            location.reload();
+        }, 25000);
+        function resizeTextarea() {
+            const textarea = document.querySelector('.config-field');
+            textarea.style.height = 'auto';
+            textarea.style.height = textarea.scrollHeight - textarea.value.split('\n').length * 1.3 + 'px';
         }
 
-        try:
-            response = requests.post(config['server_url'] + '/upload', files=files, data=data, timeout=5)
-            if response.status_code == 200:
-                if response.text == 'You need to register first!':
-                    print('Error: You need to register first!')
-                else:
-                    print('Image uploaded successfully!')
-            else:
-                print('Failed to upload image:', response.text)
-        except (Timeout, ConnectionError):
-            print('Error: Cant connect to Server')
-        except InvalidSchema:
-            print('Error: Invalid server IP.')
+        function updateConfig() {
+            const textarea = document.querySelector(".config-field");
+            const content = textarea.value;
+            fetch("/update_config.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({content}),
+            })
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+        }
+    </script>
 
-    except PermissionError:
-        print("Error: Permission denied. USB drive not found.")
+ <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: #141414;
+            font-family: Consolas, sans-serif;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            box-sizing: border-box;
+            background-color: #FFFFFF;
+            border-radius: 10px;
+            box-shadow: 0px 2px 5px rgba(0,0,0,0.3);
+            text-align: center;
+        }
+        h1 {
+            margin-top: 0;
+            font-size: 36px;
+            color: #333333;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+        }
+        img {
+            max-width: 100%;
+            border-radius: 10px;
+            box-shadow: 0px 2px 5px rgba(0,0,0,0.3);
+            margin-top: 20px;
+        }
+        .logo {
+            position: absolute;
+            top: 20px;
+            left: 20px;
+            width: 100px;
+            height: 100px;
+            background-color: #333333;
+            border-radius: 50%;
+            box-shadow: 0px 2px 5px rgba(0,0,0,0.3);
+            color: #FFFFFF;
+            line-height: 1px;
+            font-size: 24px;
+            text-transform: uppercase;
+            background-image: url("logo.png");
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+        .logo img {
+            max-width: 100%;
+            max-height: 100%;
+            object-fit: cover;
+        }
+        .status {
+            margin-top: 20px;
+            font-size: 24px;
+            font-weight: bold;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+        }
+        .active .status {
+            color: #33CC33;
+        }
+        .stopped .status {
+            color: #FF3333;
+        }
 
-    # Schedule the deletion of the image if save_to_drive is False
-    if not config.get('save_to_drive', True):
-        def delete_image():
-            if os.path.exists(file_name):
-                os.remove(file_name)
-                print('Image deleted.')
+        button {
+            display: inline-block;
+            background-color: #333333;
+            color: #FFFFFF;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            margin-top: 20px;
+            font-size: 16px;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+            cursor: pointer;
+        }
+        button:hover {
+            background-color: #666666;
+        }
+        button:focus {
+            outline: none;
+        }
+        
+        .config-field {
+            display: flex;
+            width: 50%;
+	    min-width: 360px;
+            margin: 20px auto;
+            padding: 5px;
+            font-family: Consolas, sans-serif;
+            font-size: 14px;
+            resize: none;
+            overflow: auto;
+            height: auto;
+        }
 
-        scheduler.enter(1, 1, delete_image)
+        .update-button {
+            display: block;
+            margin: 10px auto;
+            padding: 10px 20px;
+            background-color: #333333;
+            color: #FFFFFF;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+            text-transform: uppercase;
+            letter-spacing: 1px;
+            cursor: pointer;
+        }
 
-    # Schedule the next capture after the specified delay
-    delay = int(config['sleep'])
-    scheduler.enter(delay, 1, capture_and_upload_image, (config, scheduler))
+        .update-button:hover {
+            background-color: #666666;
+        }
+
+        .config-text {
+            position: relative;
+            color: #3333FF;
+            display: inline-block;
+            width: auto;
+        }
+
+        .config-popup {
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            width: max-content;
+            background-color: #FFFFFF;
+            border-radius: 5px;
+            padding: 10px;
+            box-shadow: 0px 2px 5px rgba(0,0,0,0.3);
+            z-index: 1;
+            color: #333333;
+            display: none;
+        }
+
+        .config-text:hover .config-popup {
+            display: block;
+            width: auto;
+        }
+    </style>
 
 
-# Load the configuration file
-with open('/var/www/html/config.json', 'r') as f:
-    config = json.load(f)
+    <link rel="icon" type="image/x-icon" href="favicon.png">
+</head>
+<body>
+<div class="logo"></div>
+<div class="container">
+        <form action="control.php" method="post">
+          <?php
+            $autocam_running = shell_exec("ps -ef | grep autocam.py | grep -v grep");
+            $start_disabled = $autocam_running ? 'disabled' : '';
+          ?>
+          <button type="submit" name="action" value="start" <?php echo $start_disabled; ?>>Start</button>
+          <button type="submit" name="action" value="stop">Stop</button>
+        </form>
 
-# Initialize the scheduler
-scheduler = sched.scheduler(time.time, time.sleep)
+    <?php
+    $config_file = '/var/www/html/config.json'; // Path to the config file
 
-# Schedule the initial image capture
-scheduler.enter(0, 1, capture_and_upload_image, (config, scheduler))
+    // Function to update the config file
+    function updateConfigFile($content) {
+        $config_file = '/var/www/html/config.json';
+        file_put_contents($config_file, $content);
+    }
 
-# Run the scheduler
-scheduler.run()
+    // Handle the POST request for updating the config file
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $post_data = json_decode(file_get_contents('php://input'), true);
+        $content = $post_data['content'];
+        updateConfigFile($content);
+        echo 'Config file updated successfully!';
+        exit;
+    }
+
+    // Load the config file
+    $config = json_decode(file_get_contents($config_file), true);
+
+    $directory = $index_path . $config['img_dir']; // Set the directory where your images are stored
+
+    $autocam_running = shell_exec("ps -ef | grep autocam.py | grep -v grep");
+
+    // Get a list of all files in the directory
+    $files = glob($directory . '*.jpg');
+
+    // Sort the files by last modified date, with newest first
+    usort($files, function($a, $b) {
+        return filemtime($b) - filemtime($a);
+    });
+
+    // Check if any files were found
+    if (count($files) > 0) {
+        // Get the latest file
+        $latest_file = $files[0];
+        $latest_time = filemtime($latest_file);
+
+        // Calculate the time difference between now and the latest file
+        $diff_minutes = (time() - $latest_time) / 60;
+
+        // Determine the status text and color
+        if ($autocam_running) {
+            $status_text = 'Active!, last picture: ' . round($diff_minutes) . ' min';
+            $status_color = 'green';
+        } else {
+            $status_text = 'Not Active!, last picture: ' . round($diff_minutes) . ' min';
+            $status_color = 'red';
+        }
+
+        // Display the latest file information
+        echo '<img src="images/' . basename($latest_file) . '" alt="Latest Picture" width="500" height="300">';
+        echo '<p>Name: ' . basename($latest_file) . '</p>';
+        echo '<p>Time taken: ' . date("Y-m-d H:i:s", $latest_time) . '</p>';
+        echo '<p>Current time: ' . date("Y-m-d H:i:s") . '</p>';
+        echo '<p>Status: <span style="color: ' . $status_color . ';"><strong>' . $status_text . '</strong></span></p>';
+    } else {
+        echo '<p>No images found.</p>';
+    }
+
+    // Read the config file and display its content in a textarea for editing
+    $config_content = file_get_contents($config_file);
+    echo '<textarea class="config-field" oninput="resizeTextarea()">' . htmlspecialchars($config_content) . '</textarea>';
+
+    // Update button to save the modified config
+    echo '<button class="update-button" onclick="updateConfig(); location.reload();">&#10004;</button>';
+
+    // JavaScript function to update the config file
+    echo '<script>
+        function updateConfig() {
+            const textarea = document.querySelector(".config-field");
+            const content = textarea.value;
+            fetch("/update_config.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({content}),
+            })
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+        }
+    </script>';
+    ?>
+
+	<p>
+	  IP address: <br>
+	  <?php
+	  $ips = array();
+	  exec("/usr/sbin/ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'", $ips);
+	  foreach ($ips as $ip) {
+	    echo "$ip<br>";
+	  }
+	  ?>
+	</p>
+</div>
+</body>
+</html>
