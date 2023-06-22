7a3e5aa648a1aef5a018ee4eab697a998cb72380
Neptun <128908840+nanocraftmr@users.noreply.github.com>
2023-06-22 14:36:31 +0200

    clean config editing

--- index.php
+++ index.php
@@ -15,12 +15,32 @@
         function updateConfig() {
             const textarea = document.querySelector(".config-field");
             const content = textarea.value;
+
+            // Get the current value of the save_to_drive switch
+            const saveToDriveSwitch = document.querySelector("#save-to-drive-switch");
+            const saveToDriveValue = saveToDriveSwitch.checked;
+
+            // Get the values of sleep and key inputs
+            const sleepInput = document.querySelector("#sleep-input");
+            const sleepValue = sleepInput.value;
+            const keyInput = document.querySelector("#key-input");
+            const keyValue = keyInput.value;
+
+            // Update the config object
+            const config = JSON.parse(content);
+            config.save_to_drive = saveToDriveValue;
+            config.sleep = sleepValue;
+            config.key = keyValue;
+
+            // Convert the updated config object back to JSON
+            const updatedContent = JSON.stringify(config, null, 4);
+
             fetch("/update_config.php", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify({content}),
+                body: JSON.stringify({content: updatedContent}),
             })
             .then(response => response.text())
             .then(data => {
@@ -32,7 +52,7 @@
         }
     </script>
 
- <style>
+    <style>
         body {
             margin: 0;
             padding: 0;
@@ -122,7 +142,7 @@
         .config-field {
             display: flex;
             width: 50%;
-	    min-width: 360px;
+	          min-width: 360px;
             margin: 20px auto;
             padding: 5px;
             font-family: Consolas, sans-serif;
@@ -177,122 +197,148 @@
         }
     </style>
 
-
     <link rel="icon" type="image/x-icon" href="favicon.png">
 </head>
 <body>
-<div class="logo"></div>
-<div class="container">
+    <div class="logo"></div>
+    <div class="container">
         <form action="control.php" method="post">
-          <?php
-            $autocam_running = shell_exec("ps -ef | grep autocam.py | grep -v grep");
-            $start_disabled = $autocam_running ? 'disabled' : '';
-          ?>
-          <button type="submit" name="action" value="start" <?php echo $start_disabled; ?>>Start</button>
-          <button type="submit" name="action" value="stop">Stop</button>
+            <?php
+                $autocam_running = shell_exec("ps -ef | grep autocam.py | grep -v grep");
+                $start_disabled = $autocam_running ? 'disabled' : '';
+            ?>
+            <button type="submit" name="action" value="start" <?php echo $start_disabled; ?>>Start</button>
+            <button type="submit" name="action" value="stop">Stop</button>
         </form>
 
-    <?php
-    $config_file = '/var/www/html/config.json'; // Path to the config file
+        <?php
+        $config_file = '/var/www/html/config.json'; // Path to the config file
 
-    // Function to update the config file
-    function updateConfigFile($content) {
-        $config_file = '/var/www/html/config.json';
-        file_put_contents($config_file, $content);
-    }
+        // Function to update the config file
+        function updateConfigFile($content) {
+            $config_file = '/var/www/html/config.json';
+            file_put_contents($config_file, $content);
+        }
 
-    // Handle the POST request for updating the config file
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $post_data = json_decode(file_get_contents('php://input'), true);
-        $content = $post_data['content'];
-        updateConfigFile($content);
-        echo 'Config file updated successfully!';
-        exit;
-    }
+        // Handle the POST request for updating the config file
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $post_data = json_decode(file_get_contents('php://input'), true);
+            $content = $post_data['content'];
+            updateConfigFile($content);
+            echo 'Config file updated successfully!';
+            exit;
+        }
 
-    // Load the config file
-    $config = json_decode(file_get_contents($config_file), true);
+        // Load the config file
+        $config = json_decode(file_get_contents($config_file), true);
 
-    $directory = $index_path . $config['img_dir']; // Set the directory where your images are stored
+        $directory = $index_path . $config['img_dir']; // Set the directory where your images are stored
 
-    $autocam_running = shell_exec("ps -ef | grep autocam.py | grep -v grep");
+        $autocam_running = shell_exec("ps -ef | grep autocam.py | grep -v grep");
 
-    // Get a list of all files in the directory
-    $files = glob($directory . '*.jpg');
+        // Get a list of all files in the directory
+        $files = glob($directory . '*.jpg');
 
-    // Sort the files by last modified date, with newest first
-    usort($files, function($a, $b) {
-        return filemtime($b) - filemtime($a);
-    });
+        // Sort the files by last modified date, with newest first
+        usort($files, function($a, $b) {
+            return filemtime($b) - filemtime($a);
+        });
 
-    // Check if any files were found
-    if (count($files) > 0) {
-        // Get the latest file
-        $latest_file = $files[0];
-        $latest_time = filemtime($latest_file);
+        // Check if any files were found
+        if (count($files) > 0) {
+            // Get the latest file
+            $latest_file = $files[0];
+            $latest_time = filemtime($latest_file);
 
-        // Calculate the time difference between now and the latest file
-        $diff_minutes = (time() - $latest_time) / 60;
+            // Calculate the time difference between now and the latest file
+            $diff_minutes = (time() - $latest_time) / 60;
 
-        // Determine the status text and color
-        if ($autocam_running) {
-            $status_text = 'Active!, last picture: ' . round($diff_minutes) . ' min';
-            $status_color = 'green';
+            // Determine the status text and color
+            if ($autocam_running) {
+                $status_text = 'Active!, last picture: ' . round($diff_minutes) . ' min';
+                $status_color = 'green';
+            } else {
+                $status_text = 'Not Active!, last picture: ' . round($diff_minutes) . ' min';
+                $status_color = 'red';
+            }
+
+            // Display the latest file information
+            echo '<img src="images/' . basename($latest_file) . '" alt="Latest Picture" width="500" height="300">';
+            echo '<p>Name: ' . basename($latest_file) . '</p>';
+            echo '<p>Time taken: ' . date("Y-m-d H:i:s", $latest_time) . '</p>';
+            echo '<p>Current time: ' . date("Y-m-d H:i:s") . '</p>';
+            echo '<p>Status: <span style="color: ' . $status_color . ';"><strong>' . $status_text . '</strong></span></p>';
         } else {
-            $status_text = 'Not Active!, last picture: ' . round($diff_minutes) . ' min';
-            $status_color = 'red';
+            echo '<p>No images found.</p>';
         }
 
-        // Display the latest file information
-        echo '<img src="images/' . basename($latest_file) . '" alt="Latest Picture" width="500" height="300">';
-        echo '<p>Name: ' . basename($latest_file) . '</p>';
-        echo '<p>Time taken: ' . date("Y-m-d H:i:s", $latest_time) . '</p>';
-        echo '<p>Current time: ' . date("Y-m-d H:i:s") . '</p>';
-        echo '<p>Status: <span style="color: ' . $status_color . ';"><strong>' . $status_text . '</strong></span></p>';
-    } else {
-        echo '<p>No images found.</p>';
-    }
+        // Read the config file and display its content in a textarea for editing
+//        $config_content = file_get_contents($config_file);
+//        echo '<textarea class="config-field" oninput="resizeTextarea()">' . htmlspecialchars($config_content) . '</textarea>';
 
-    // Read the config file and display its content in a textarea for editing
-    $config_content = file_get_contents($config_file);
-    echo '<textarea class="config-field" oninput="resizeTextarea()">' . htmlspecialchars($config_content) . '</textarea>';
+        // Switch for "save_to_drive" option
+        echo '<div class="config-switch">';
+        echo '<label class="config-text" for="save-to-drive-switch">Save to Drive</label>';
+        echo '<label class="switch">';
+        echo '<input id="save-to-drive-switch" type="checkbox" ' . ($config['save_to_drive'] ? 'checked' : '') . '>';
+        echo '<span class="slider"></span>';
+        echo '</label>';
+        echo '</div>';
 
-    // Update button to save the modified config
-    echo '<button class="update-button" onclick="updateConfig(); location.reload();">&#10004;</button>';
+        // JavaScript function to update the config file
+        echo '<script>
+          function updateConfig() {
+              const textarea = document.querySelector(".config-field");
+              const content = textarea.value;
+          
+              // Get the current value of the save_to_drive switch
+              const saveToDriveSwitch = document.querySelector("#save-to-drive-switch");
+              const saveToDriveValue = saveToDriveSwitch.checked;
+          
+              // Get the values of sleep and key inputs
+              const sleepInput = document.querySelector("#sleep-input");
+              const sleepValue = sleepInput.value;
+              const keyInput = document.querySelector("#key-input");
+              const keyValue = keyInput.value;
+          
+              // Update the config object
+              const config = JSON.parse(content);
+              config.save_to_drive = saveToDriveValue;
+              config.sleep = sleepValue;
+              config.key = keyValue;
+          
+              // Convert the updated config object back to JSON
+              const updatedContent = JSON.stringify(config, null, 4);
+          
+              fetch("/update_config.php", {
+                  method: "POST",
+                  headers: {
+                      "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({content: updatedContent}),
+              })
+              .then(response => response.text())
+              .then(data => {
+                  console.log(data);
+                  location.reload();
+              })
+              .catch(error => {
+                  console.error("Error:", error);
+              });
+          }
+        </script>';
+        ?>
+        
+      <div class="config-input">
+          <label for="sleep-input">Sleep:</label>
+          <input id="sleep-input" type="number" value="<?php echo $config['sleep']; ?>">
+  
+          <label for="key-input">Key:</label>
+          <input id="key-input" type="text" value="<?php echo $config['key']; ?>">
+      </div>        
+      
+      <button class="update-button" onclick="updateConfig(); location.reload();">&#10004;</button>
 
-    // JavaScript function to update the config file
-    echo '<script>
-        function updateConfig() {
-            const textarea = document.querySelector(".config-field");
-            const content = textarea.value;
-            fetch("/update_config.php", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({content}),
-            })
-            .then(response => response.text())
-            .then(data => {
-                console.log(data);
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
-        }
-    </script>';
-    ?>
-
-	<p>
-	  IP address: <br>
-	  <?php
-	  $ips = array();
-	  exec("/usr/sbin/ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'", $ips);
-	  foreach ($ips as $ip) {
-	    echo "$ip<br>";
-	  }
-	  ?>
-	</p>
-</div>
+    </div>
 </body>
 </html>
